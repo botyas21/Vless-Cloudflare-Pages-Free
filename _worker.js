6da49114242142c5f2dc27265dd61b9506c66b60
mohsen <144608092+botyas21@users.noreply.github.com>
2024-01-20 20:17:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = 'd01c74d8-daf2-40f9-8183-d65280f3704a';
+let userID = '46f46470-91a2-4342-b2ab-49005a999b93';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
